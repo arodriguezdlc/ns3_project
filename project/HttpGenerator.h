b98d5a239d308f07c9c5c5b845062f506b082953
Alberto <albertrc.93@gmail.com>
2016-01-04 19:30:43 +0100

    Add class structure

---
+++ project/HttpGenerator.h
@@ -0,0 +1,21 @@
+class HttpGenerator : public Application
+{
+  public:
+
+  private:
+    //Private attributes
+
+    //Private methods
+    virtual void StartApplication (void) {
+
+    }
+
+    virtual void StopApplication (void) {
+
+    }
+
+    void DoGenerate (void) {
+
+    }
+
+};
