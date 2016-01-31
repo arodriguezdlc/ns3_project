de9ed8630405275b1ebe9f30ffc116273ee39ba3
Alberto <albertrc.93@gmail.com>
2016-01-31 12:49:25 +0100

    Simulation for test multiple http clients

--- project/simulation.cc
+++ project/simulation.cc
@@ -7,7 +7,8 @@
 #include "ns3/applications-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
+//#include "ns3/point-to-point-module.h"
+#include "ns3/csma-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
 #include "HttpGeneratorClientHelper.h"
 #include "HttpGeneratorClient.h"
@@ -22,6 +23,8 @@ NS_LOG_COMPONENT_DEFINE ("simulation");
 
 int main (int argc, char *argv[]) 
 {
+    GlobalValue::Bind("ChecksumEnabled", BooleanValue(true));
+    Time::SetResolution (Time::US);
 
     //Default values
     DataRate datarate("100Mbps");
@@ -45,14 +48,14 @@ int main (int argc, char *argv[])
 
     //Creating client nodes (client and server)
     NodeContainer nodes;
-    nodes.Create (2);
+    nodes.Create (3);
 
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute ("DataRate", DataRateValue(datarate));
-    pointToPoint.SetChannelAttribute ("Delay", TimeValue(delay));
+    CsmaHelper csma;
+    csma.SetChannelAttribute ("DataRate", DataRateValue(datarate));
+    csma.SetChannelAttribute ("Delay", TimeValue(delay));
 
     NetDeviceContainer devices;
-    devices = pointToPoint.Install (nodes);
+    devices = csma.Install (nodes);
 
     //Install the internet stack on the nodes
     InternetStackHelper internet;
@@ -67,23 +70,23 @@ int main (int argc, char *argv[])
     *******************************/
 
     //HERE WE HAVE TO INSTALL CLIENT APP
-    HttpGeneratorClientHelper httpClient ("ns3::TcpSocketFactory", InetSocketAddress (interfaces.GetAddress (1), PORT));        
+    HttpGeneratorClientHelper httpClient ("ns3::TcpSocketFactory", InetSocketAddress (interfaces.GetAddress (2), PORT));        
     ApplicationContainer httpClientApp1 = httpClient.Install (nodes.Get (0));
-    //ApplicationContainer httpClientApp2 = httpClient.Install (nodes.Get (1));
+    ApplicationContainer httpClientApp2 = httpClient.Install (nodes.Get (1));
 
     httpClientApp1.Start (Seconds(1.0));
     httpClientApp1.Stop  (Seconds(60.0)); 
-    //httpClientApp2.Start (Seconds(1.0));
-    //httpClientApp2.Stop  (Seconds(60.0)); 
+    httpClientApp2.Start (Seconds(1.0));
+    httpClientApp2.Stop  (Seconds(60.0)); 
 
     //HERE WE HAVE TO INSTALL SERVER APP
     HttpGeneratorServerHelper httpServer ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), PORT));
-    ApplicationContainer httpServerApp = httpServer.Install (nodes.Get (1));
+    ApplicationContainer httpServerApp = httpServer.Install (nodes.Get (2));
     httpServerApp.Start (Seconds (1.0));
     httpServerApp.Stop  (Seconds (60.0));
 
     if (tracing) {
-       pointToPoint.EnablePcapAll ("httpGenerator");
+       csma.EnablePcapAll ("httpGenerator");
     }
     /*************************
     * Simulation execution   *
