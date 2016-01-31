8a0afc3749a0fd0a4215f785b0a4c94971d7130c
erngarcar <egarciac1991@gmail.com>
2016-01-31 12:03:23 +0100

    Creo el escenario y empiezo a instalar aplicaciones

--- project/simulation.cc
+++ project/simulation.cc
@@ -5,7 +5,6 @@
 #include <ns3/point-to-point-net-device.h>
 #include <ns3/point-to-point-channel.h>
 #include <ns3/drop-tail-queue.h>
-#include "BitAlternante.h"
 
 
 
@@ -21,34 +20,93 @@ main (int argc, char *argv[])
 
   // Parametros de la simulacion
 
-  uint32_t nG711 = 2;
+  uint32_t nVoip = 2;
   uint32_t nHttpClient = 3;
 
   // Preparar los parametros
 
-  cmd.AddValue ("G711", "Número de nodos VoIP", nG711);
+  cmd.AddValue ("Voip", "Número de nodos VoIP", nVoip);
   cmd.AddValue ("HttpClient", "Número de nodos cliente HTTP", nHttpClient);
   cmd.Parse (argc,argv);
 
-  // Componentes del escenario:
- 
-  NodeContainer G711Nodes;
-  G771Nodes.create(nG711);
+  /*********************
+  * Creacion escenario *
+  **********************/
+
+  // Nodos VoIP y Http
+  NodeContainer VoipNodes;
+  VoipNodes.create(nVoip);
   NodeContainer HttpClientNodes;
   HttpClientNodes.create(nHttpClient);
 
-  // Creamos las aplicaciones
-  // Aplicacion para VoIP
+  // Nodos que pertenecen al enlace punto a punto
+  NodeContainer p2pNodes;
+  p2pNodes.Create (2);
 
-  G711Generator AplicationVoIP();
+  // Nodos que pertenecen al enlace csma
+  NodeContainer csmaNodes;
+  csmaNodes.Add (p2pNodes.Get (1));
+  csmaNodes.Add (VoipNodes);
+  csmaNodes.Add (HttpClientNodes);
 
-  // Aplicacion para cliente Http
+  // Instalamos el dispositivo en los nodos punto a punto
+  PointToPointHelper pointToPoint;
+  NetDeviceContainer p2pDevices;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("2Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  p2pDevices = pointToPoint.Install (p2pNodes);
 
-  HttpGeneratorClient AplicationHttpClient();
+  // Instalamos el dispositivo de red en los nodos csma
+  CsmaHelper csma;
+  NetDeviceContainer csmaDevices;
+  csma.SetChannelAttribute ("DataRate", StringValue("100Mbps"));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+  csmaDevices = csma.Install (csmaNodes);
 
-  // Apliacion para servidor Http
+  // Instalamos la pila TCP/IP en todos los nodos
+  InternetStackHelper stack;
+  stack.Install (p2pNodes.Get (0));
+  stack.Install (csmaNodes);
 
-  HttpGeneratorServer AplicationHttpServer();
+  // Asignamos direcciones a cada una de las interfaces
+  // Utilizamos dos rangos de direcciones diferentes:
+  //    - un rango para los dos nodos del enlace
+  //      punto a punto
+  //    - un rango para los nodos de la red de área local.
+  Ipv4AddressHelper address;
+  Ipv4InterfaceContainer p2pInterfaces;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  p2pInterfaces = address.Assign (p2pDevices);
+  Ipv4InterfaceContainer csmaInterfaces;
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  csmaInterfaces = address.Assign (csmaDevices);
+
+  // Calculamos las rutas del escenario. Con este comando, los
+  //     nodos de la red de área local definen que para acceder
+  //     al nodo del otro extremo del enlace punto a punto deben
+  //     utilizar el primer nodo como ruta por defecto.
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  /*******************************
+   * Instalacion de aplicaciones *
+   *******************************/
+
+  // Instalacion de aplicacion VoIP
+  uint16_t port = 9;
+  PacketSinkHelper sink ("ns3::UdpSocketFactory", Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+  ApplicationContainer app = sink.Install (p2pNodes.Get (0));
+
+  G711Generator VoIPapp;
+  VoIPapp.SetRemote("ns3::UdpSocketFactory",serverAddress);
+  for(uint32_t i = 0 ; i < nVoip ; i++)
+    nodes.Get(i)->AddApplication(&VoIPapp);
+  
+  VoIPapp.Start (Seconds (1.0));
+  VoIPapp.Stop (Seconds (10.0));
+  
+  // Instalacion de aplicacion HttpGenerator
+  
+
 
   //*********************************************************************************************************************
 
